94f0e61db05ed6d39dc08561f353ae3cabbbcf7c
sammy2201 <bhavanish2000@gmail.com>
2024-03-03 17:53:52 +0100

    changes made1

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -25,7 +25,7 @@ const Home = () => {
       <Ournavbar typeOfUser_inserver={typeOfUser_inserver} />
       <h2 className="mainheading">
         Quizzes that inspire, enlighten, and transform curiosity into wisdom.
-        Unlock knowledge, one question at a time
+        Unlock knowledge, one question at a time.
       </h2>
       <div className="imagediv">
         <img className="mainimage" src={require("../images/education.svg")} />
